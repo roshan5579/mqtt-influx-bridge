99ff427fcb8b8232994290cdf8d2aeb89a00cb60
roshan5579 <117844871+roshan5579@users.noreply.github.com>
2026-03-07 11:47:13 +0530

    Refactor MQTT data handling and InfluxDB config
    Removed old InfluxDB configuration and updated the message handling to use a new line format for sending data.

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
 const mqtt = require("mqtt");
 const axios = require("axios");
 
-// ================= MQTT CONFIG =================
 const MQTT_BROKER = "mqtts://01792b66dfee4540a546dc894922fb94.s1.eu.hivemq.cloud:8883";
 const MQTT_TOPIC = "tractor/data";
+
+const INFLUX_URL = "https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/write?org=Moonrider%20Pvt%20Ltd&bucket=Tractor_Data&precision=s";
+
+const TOKEN = "YOUR_TOKEN";
 
 const client = mqtt.connect(MQTT_BROKER,{
   username: "MR_TRACTOR",
   password: "#Lokesh000"
 });
 
-// ================= INFLUXDB CONFIG =================
-const INFLUX_URL = "https://eu-central-1-1.aws.cloud2.influxdata.com/api/v2/write?org=Moonrider%20Pvt%20Ltd&bucket=Tractor_Data&precision=s";
-
-const TOKEN = "BLjKjM_zGG5u2HJkw1uPusEjzSjbdYee8JvC_TqUnS-21z6ckYoIoXrJH8Dc3TmeGm2Vr71uQoB5xs3Uz69y8w==";
-
-// ================= MQTT CONNECT =================
 client.on("connect", () => {
-
   console.log("Connected to HiveMQ");
-
   client.subscribe(MQTT_TOPIC);
-
 });
 
-// ================= MQTT DATA =================
 client.on("message", async (topic, message) => {
 
-  console.log("Received:", message.toString());
+  const line = message.toString();
+
+  console.log("Received:", line);
 
   try {
-
-    const data = JSON.parse(message.toString());
-
-    const line = `tractor voltage=${data.voltage},current=${data.current}`;
 
     await axios.post(INFLUX_URL, line, {
       headers:{
@@ -42,11 +33,11 @@ client.on("message", async (topic, message) => {
       }
     });
 
-    console.log("Data sent to InfluxDB");
+    console.log("Data written to InfluxDB");
 
   } catch(err) {
 
-    console.log("Error:", err);
+    console.log("InfluxDB error:", err.response?.data || err.message);
 
   }
 
